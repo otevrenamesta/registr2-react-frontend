7407d59108b7274b384f5e815ce32a364916aab8
Vaclav Klecanda <vencax77@gmail.com>
2016-08-22 23:47:27 +0200

    port configurable

--- fake_backend/index.js
+++ fake_backend/index.js
@@ -4,9 +4,10 @@ var jsonServer = require('json-server')
 var server = jsonServer.create()
 var router = jsonServer.router(data)
 var middlewares = jsonServer.defaults()
+var port = process.env.PORT || 3000
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, function () {
-  console.log('JSON Server is running')
+server.listen(port, function () {
+  console.log('JSON Server is running on ' + port)
 })
